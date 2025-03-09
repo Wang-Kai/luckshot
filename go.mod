e454f06a98940fdc4350fc8c06b4f2b3bdcff96b
steve <persistence2013@sina.com>
2025-03-09 12:20:56 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Wang-Kai/luckshot
+
+go 1.24.1
---
+++ luckshot.go
@@ -0,0 +1,19 @@
+package luckshot
+
+import "math/rand"
+
+type LuckShot struct {
+	shot bool
+}
+
+func (ls *LuckShot) Do(fn func()) {
+	if ls.shot {
+		fn()
+	}
+}
+
+func Chance(base, target int) *LuckShot {
+	randNum := rand.Intn(base)
+	shot := randNum < target
+	return &LuckShot{shot}
+}
